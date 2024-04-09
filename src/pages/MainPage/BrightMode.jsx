e60af1d2d06f572c06b9db05ec83c8d19f415287
SnowsFE <snoerkr@gmail.com>
2024-04-09 15:40:12 +0900

    4월 9일

--- src/pages/MainPage/BrightMode.jsx
+++ src/pages/MainPage/BrightMode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled, { keyframes, createGlobalStyle } from "styled-components";
 
 // 버튼 애니메이션
@@ -21,7 +21,7 @@ const Button = styled.button`
   cursor: pointer;
   font-size: 24px;
   position: absolute;
-  top: 5px;
+  top: 5px; // 버튼을 색상 선택기 바로 아래로 이동
   right: 20px;
   color: white;
   padding: 5px 10px;
@@ -36,30 +36,54 @@ const Button = styled.button`
   );
   background-size: 200% 200%;
   animation: ${glow} 3s ease infinite;
+  z-index: 100;
+`;
+
+// 색상 선택기 스타일
+const HiddenColorPicker = styled.input`
+  opacity: 0;
+  position: absolute;
+  top: 5px; // 원래 버튼 위치에 색상 선택기를 배치
+  right: 20px;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  z-index: -1;
 `;
 
 // 전역 스타일 설정
 const GlobalStyle = createGlobalStyle`
   body {
     transition: background-color 0.3s ease;
-    background-color: ${(props) => (props.isDarkMode ? "#4a5751" : "#ffffff")};
+    background-color: ${(props) => props.backgroundColor || "#ffffff"};
   }
 `;
 
-const DarkLightModeButton = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false); // 다크 모드 상태 관리
+const BrightMode = () => {
+  const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+  const [buttonEmoji, setButtonEmoji] = useState("🎨"); // 버튼 이모지 상태 관리
+  const colorPickerRef = useRef(null);
 
-  // 다크 모드 상태 토글 함수
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
+  const handleGlassesClick = () => {
+    colorPickerRef.current.click();
+  };
+
+  const handleChangeBackgroundColor = (e) => {
+    setBackgroundColor(e.target.value);
+    setButtonEmoji("👓"); // 배경색이 변경되면 버튼 이모지를 로 변경
   };
 
   return (
     <>
-      <GlobalStyle isDarkMode={isDarkMode} />
-      <Button onClick={toggleDarkMode}>{isDarkMode ? "🕶️" : "👓"}</Button>
+      <GlobalStyle backgroundColor={backgroundColor} />
+      <HiddenColorPicker
+        type="color"
+        ref={colorPickerRef}
+        onChange={handleChangeBackgroundColor}
+      />
+      <Button onClick={handleGlassesClick}>{buttonEmoji}</Button>
     </>
   );
 };
 
-export default DarkLightModeButton;
+export default BrightMode;
