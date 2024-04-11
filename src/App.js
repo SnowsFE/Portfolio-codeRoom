733a5d4d2427cf3b549a3ac7912d728385df4052
won-jae-jang <dnjswo410@naver.com>
2024-04-11 14:20:10 +0900

    1차 merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
 import InfoPage from "./pages/infopage/InfoPage";
 import LoginPage from "./pages/loginpage/LoginPage";
@@ -6,29 +5,21 @@ import JoinPage from "./pages/loginpage/JoinPage";
 import DetailPage from "./pages/detailpage/DetailPage";
 import WritePage from "./pages/writepage/WritePage";
 
-=======
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage.jsx";
->>>>>>> NewDoHyeon
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Routes>
-        <Route path="/" element={<InfoPage />}></Route>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/info" element={<InfoPage />}></Route>
         <Route path="/users/login" element={<LoginPage />}></Route>
         <Route path="/users/join" element={<JoinPage />}></Route>
         <Route path="/detail/:id" element={<DetailPage />}></Route>
         <Route path="/boards" element={<WritePage />}></Route>
       </Routes>
-=======
-      <Router>
-        <MainPage />
-      </Router>
->>>>>>> NewDoHyeon
     </div>
   );
 }
--- src/pages/MainPage/NewBoards.jsx
+++ src/pages/MainPage/NewBoards.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const NewBoards = (props) => {
