3534dfad4c6788a4c3777755c5a0a6c63a1d49dc
맹주영 <wndud6302@naver.com>
2024-04-19 09:31:08 +0900

    session

--- src/config/database/db_config.js
+++ src/config/database/db_config.js
@@ -17,7 +17,7 @@ const dbInfo = mysql.createPool({
     password: '1234',
 
     // database: 연결하려는 데이터베이스(schemas)의 이름입니다.
-    database: 'coderoom'
+    database: 'aaa'
 });
 
 // 생성된 커넥션 풀을 모듈로 내보냅니다. 이를 통해 다른 파일에서 이 커넥션 풀을 불러와 데이터베이스 작업을 수행할 수 있습니다.
