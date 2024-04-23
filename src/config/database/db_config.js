a40a8f8b90edf85c4dbc952db69f8b003113c8be
ura <guddk3434@gmail.com>
2024-04-24 08:58:27 +0900

    풀땡기기

--- src/config/database/db_config.js
+++ src/config/database/db_config.js
@@ -17,7 +17,10 @@ const dbInfo = mysql.createPool({
     password: 'akdlakdl12!',
 
     // database: 연결하려는 데이터베이스(schemas)의 이름입니다.
-    database: 'coderoom'
+    database: 'coderoom',
+
+    // 이모티콘 글 쓰기 삽입 시 정상 출력을 위한 코드
+    charset: 'utf8mb4',
 });
 
 // 생성된 커넥션 풀을 모듈로 내보냅니다. 이를 통해 다른 파일에서 이 커넥션 풀을 불러와 데이터베이스 작업을 수행할 수 있습니다.
